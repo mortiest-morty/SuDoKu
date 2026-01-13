87bb10b1c89ec4e8a030abba67603ce518d1ec8b
mortiest-morty <mortyyy13@gmail.com>
2026-01-13 18:15:21 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "lul.h"
+
+using namespace std;
+
+int main() {
+    setColor(0, 11);
+
+    setlocale(LC_ALL, "rus");
+
+    int n = 9, m = 9;
+    int a[9][9] = {};
+    string begin = "(^-^) ====== S U D O K U ====== (^-^)";
+    string continue_file, filename;
+
+    //printScreensaver();
+
+    cout << endl;
+
+    printMenu(a, begin);
+
+    return 0;
+}
